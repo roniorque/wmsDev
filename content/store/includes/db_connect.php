543020e660b90e547b40fca243ceb362935b9f86
Ronio Jerico Takahashi <roniojericho144@yahoo.com>
2024-01-26 23:38:35 +0800

    changed config

--- content/store/includes/db_connect.php
+++ content/store/includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 define ("DB_HOST", "localhost");
-define ("DB_USER", "root");
-define ("DB_PASS", "admin123");
-define ("DB_NAME", "php");
+define ("DB_USER", "u195495796_cody");
+define ("DB_PASS", "Adminn123*");
+define ("DB_NAME", "u195495796_wmsdemo");
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if($db->connect_errno > 0){
 die("Unable to connect to database [".$db->connect_error."]");
